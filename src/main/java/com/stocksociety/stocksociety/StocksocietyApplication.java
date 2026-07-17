438838889866ea8eb24d613a1e3d9747c175c9c0
Princess Krish Anne Bernardino <pkabernardino@gmail.com>
2026-07-17 00:00:24 -0400

    Add Spring Boot project structure

---
+++ src/main/java/com/stocksociety/stocksociety/StocksocietyApplication.java
@@ -0,0 +1,13 @@
+package com.stocksociety.stocksociety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksocietyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksocietyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stocksociety/stocksociety/StocksocietyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stocksociety.stocksociety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksocietyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
